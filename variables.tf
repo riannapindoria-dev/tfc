1237d8769984bc8ada7414ea52cdbc84e9d83cb2
riannapindoria-dev <68743830+riannapindoria-dev@users.noreply.github.com>
2020-07-31 08:39:28 +0100

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "access_key" {
+  description = "AKIAJNCRIWYUN5JAAXZA"
+  default     = ""
+}
+variable "secret_key" {
+  description = "yl1N4+fuNZq20m7zvyUNnPTS9oe2cZAWkjOBQtMp"
+  default     = ""
+}
